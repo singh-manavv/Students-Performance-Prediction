73210061a47e0d500fc48d52af0aa6efdaca5761
Manvendra Singh <55807344+singh-manavv@users.noreply.github.com>
2024-02-15 03:51:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def predict_datapoint():
         return render_template('index.html', results=results[0])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',debug=True)
